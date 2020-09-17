b48e3c5845940a07d3b0e0f57f85a09bf8055cc8
shailenn <48827151+shailenn@users.noreply.github.com>
2020-09-17 17:25:06 +0530

    Add files via upload

--- Play_Store/PS1.py
+++ Play_Store/PS1.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
+#from time import sleep, time, timezone
 
 class PlayStore:
 
@@ -30,12 +31,8 @@ class PlayStore:
     def get_data(self):
         print('Hello')
         driver = self.driver
-        #driver = webdriver.Chrome(executable_path=r"C:\Users\shailendra bisht\Downloads\chromedriver")
-        #driver.maximize_window()
-        #baseUrl = 'https://play.google.com/store/apps/details?id=com.msf.kotak&hl=en_IN&showAllReviews=true'
-        #driver.get(baseUrl)
 
-        for i in range(1):
+        for i in range(192):
             c = self.traverse()
 
         scrolls = 5
@@ -46,63 +43,37 @@ class PlayStore:
             if scrolls < 0:
                 break
 
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        reviews = WebDriverWait(driver, 30).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//*[@class='UD7Dzf']")))
+        names = soup.find_all('span', class_ = 'X43Kjb')
+        dates = soup.find_all('span', class_ = "p2TkOb")
+        ratings = soup.find_all('div', class_= 'pf5lIe')
+        reviews = soup.find_all('div', class_ = "UD7Dzf")
 
-        names = WebDriverWait(driver, 30).until(
-            #.EC.presence_of_all_elements_located((By.XPATH, "//*[@class='X43Kjb']")))
+        r = 1
+        d = 0
+        n = 0
+        re = 0
 
-        dates = WebDriverWait(driver, 30).until(
-            #.EC.presence_of_all_elements_located((By.XPATH, "//*[@class='p2TkOb']")))
+        len_ratings = len(ratings)
 
-        ratings = WebDriverWait(driver, 30).until(
-            #.EC.presence_of_all_elements_located((By.XPATH, "//*[@class='pf5lIe'].find_next()['aria-label']")))
+        file  = open("reviews.csv","w+")
+        while r < len_ratings:
+            try:
+                while str(names[n].text).find("Kotak") != -1:
+                    d += 1
+                    n += 1
+                file.write(",".join([names[n].text, ratings[r].next_element["aria-label"], dates[d].text, reviews[re].text]))
+                file.write('\n')
+            except:
+                pass
 
+            r += 1
+            d += 1
+            n += 1
+            re += 1
 
-
-        #reviews = driver.find_elements_by_xpath("//*[@class='UD7Dzf']")
-        l1 = list()
-        l2 = list()
-        l3 = list()
-        l4 = list()
-
-        for review in reviews:
-            l1.append(review.text)
-        #print(l1)
-
-        for name in names:
-            l2.append(name.text) #Odd and even separated below
-        #print(l2)
-
-        for date in dates:
-            l3.append(date.text)
-        #print(l3)
-
-        for rating in ratings:
-            l4.append(rating.text)
-        print(l4)
-
-        #Remove even_indexed element from thelist
-        odd_i = []
-        even_i = []
-        for i in range(0, len(l2)):
-            if i % 2:
-                even_i.append(l2[i]) # List of Kotak
-            else :
-                odd_i.append(l2[i])  #List of names
-
-        res = odd_i + even_i
-        #print("Seprated odd and even index list: " + str(res))
-
-        # d15Mdf bAhLNe
-        #check = driver.find_elements(By.CLASS_NAME, 'X43Kjb')
-        #print(check)
-
-        no_of_reviewers = len(driver.find_elements(By.CLASS_NAME, 'd15Mdf'))
-        print(no_of_reviewers)
-
-
+        time.sleep(5)
 
 def main():
     a = 10
@@ -119,4 +90,4 @@ def main():
 if __name__ == '__main__':
     P = PlayStore()
     P.get_data()
-    #P.traverse()
+    P.traverse()
