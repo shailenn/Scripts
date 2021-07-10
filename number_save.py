a38686d97fa06fbda739989fba595db74d747ffa
shailenn <48827151+shailenn@users.noreply.github.com>
2021-07-10 17:16:03 +0530

    Add files via upload

--- number_save.py
+++ number_save.py
@@ -1,16 +1,11 @@
-count = 4144
+count = 5200
 with open("new_nos1.txt") as f:
-    for line in f:
-        count = count + 1
-        l = line.split()
+
+	for line in f:
+		count += 1
+		l = line.strip()
+		a = l
 		
-        contact = l[1]+l[-1]
-
-        intcontact = int(contact)
-
-        sc1 = contact[0:2]
-        sc2 = contact[2:4]
-        sc3 = contact[4:-1]
-
-        with open("00009.txt", "a+") as f1:
-            f1.write('BEGIN:VCARD\nVERSION:2.1\nN:;%d;;;\nFN:%d\nTEL;CELL:%s %s %s\nTEL;CELL:%d\nEND:VCARD\n'%(count,count,sc1,sc2,sc3,intcontact))
+		with open("00009.txt", "a+") as f1:
+			
+			f1.write('BEGIN:VCARD\nVERSION:3.0\nN:;%d;;;\nFN:%d\nTEL;TYPE=CELL:%s\nEND:VCARD\n'%(count,count,a))
